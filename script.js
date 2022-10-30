7f2cf074a81a40fda9655b711bd650a45cc73137
shahrierNafis <shahriernafisog@gmail.com>
2022-10-30 16:15:41 +0600

    Add a button to reset

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
-let i = 16
-let container = document.createElement("div")
-container.id = "container"
-while (i) {
-    let j = 16
-    let row = document.createElement("div")
-    row.classList.add("row");
-    while (j) {
-        let square = document.createElement("div");
-        square.classList.add("square");
-        square.addEventListener("mouseover", changeColor)
-        row.appendChild(square);
-        j--;
+function reset(x) {
+    document.body.innerHTML = "";
+    let i = x
+    let container = document.createElement("div")
+    container.id = "container"
+    while (i) {
+        let j = x
+        let row = document.createElement("div")
+        row.classList.add("row");
+        while (j) {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            square.addEventListener("mouseover", changeColor)
+            row.appendChild(square);
+            j--;
+        }
+        container.appendChild(row);
+        i--;
     }
-    container.appendChild(row);
-    i--;
+    const button = document.createElement("button")
+    button.textContent = "Reset"
+    button.addEventListener("click", () => {
+        reset(prompt("How many squares per side? (Maximum: 100)", 64));
+    });
+    document.body.appendChild(button)
+    document.body.appendChild(container);
+    console.log(x)
 }
-document.body.appendChild(container);
-
 function changeColor() {
     this.classList.add("touched")
 }
+reset(64);
