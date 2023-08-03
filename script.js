0e0b9f171f9f0297edcbc285ff2405229bf87f66
shahrierNafis <shahriernafisog@gmail.com>
2023-08-03 19:46:59 +0600

    Upgrade changeColor Function

--- script.js
+++ script.js
@@ -1,32 +1,42 @@
 function reset(x) {
-    document.body.innerHTML = "";
-    let i = x
-    let container = document.createElement("div")
-    container.id = "container"
-    while (i) {
-        let j = x
-        let row = document.createElement("div")
-        row.classList.add("row");
-        while (j) {
-            let square = document.createElement("div");
-            square.classList.add("square");
-            square.addEventListener("mouseover", changeColor)
-            row.appendChild(square);
-            j--;
-        }
-        container.appendChild(row);
-        i--;
+  document.body.innerHTML = "";
+  let i = x;
+  let container = document.createElement("div");
+  container.id = "container";
+  while (i) {
+    let j = x;
+    let row = document.createElement("div");
+    row.classList.add("row");
+    while (j) {
+      let square = document.createElement("div");
+      square.classList.add("square");
+      square.addEventListener("mouseover", changeColor);
+      row.appendChild(square);
+      j--;
     }
-    const button = document.createElement("button")
-    button.textContent = "Reset"
-    button.addEventListener("click", () => {
-        reset(prompt("How many squares per side? (Maximum: 100)", 64));
-    });
-    document.body.appendChild(button)
-    document.body.appendChild(container);
-    console.log(x)
+    container.appendChild(row);
+    i--;
+  }
+  const button = document.createElement("button");
+  button.textContent = "Reset";
+  button.addEventListener("click", () => {
+    reset(prompt("How many squares per side? (Maximum: 100)", 64));
+  });
+  document.body.appendChild(button);
+  document.body.appendChild(container);
+  console.log(x);
 }
+var mouseDownCount = 0;
+
+document.onmousedown = function (evt) {
+  ++mouseDownCount;
+};
+document.onmouseup = function (evt) {
+  --mouseDownCount;
+};
 function changeColor() {
-    this.classList.add("touched")
+  if (mouseDownCount) {
+    this.classList.add("touched");
+  }
 }
 reset(64);
